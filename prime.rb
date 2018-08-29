1113d12b642670176974c341b5051bd1ba293b2b
Aisha D'Souza <aishad.souza@yahoo.com>
2018-08-29 10:16:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(n)
   if n > 1 
     (0..n).each do |i| 
-      if i / 2 == 0
-        return true
+      if i * 2 == n
+        return false
+        break
+      else i * 2 == n
+        return false
         break
       end
     end
